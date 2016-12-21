eae640a235100b068dcfcb19145a055213fcbf56
tama.asrory.ridhana@students.uin-suska.ac.id <tama.asrory.ridhana@students.uin-suska.ac.id>
2016-12-21 08:52:33 +0700

    SISTEM INFORMASI AKADEMIK

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=db_sia',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: asrory
+     * Date: 21/12/16
+     * Time: 6:16
+     */
+header("location:web/");
