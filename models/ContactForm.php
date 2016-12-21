8459d2995f83c0843a02247666365e24ecac8b32
tama.asrory.ridhana@students.uin-suska.ac.id <tama.asrory.ridhana@students.uin-suska.ac.id>
2016-12-21 14:41:20 +0700

    test

--- models/ContactForm.php
+++ models/ContactForm.php
@@ -1,64 +1,74 @@
 <?php
+    namespace app\models;
 
-namespace app\models;
-
-use Yii;
-use yii\base\Model;
-
-/**
- * ContactForm is the model behind the contact form.
- */
-class ContactForm extends Model
-{
-    public $name;
-    public $email;
-    public $subject;
-    public $body;
-    public $verifyCode;
-
+    use Yii;
+    use yii\base\Model;
 
     /**
-     * @return array the validation rules.
+     * ContactForm is the model behind the contact form.
      */
-    public function rules()
-    {
-        return [
-            // name, email, subject and body are required
-            [['name', 'email', 'subject', 'body'], 'required'],
-            // email has to be a valid email address
-            ['email', 'email'],
-            // verifyCode needs to be entered correctly
-            ['verifyCode', 'captcha'],
-        ];
-    }
+    class ContactForm extends Model{
+        public $name;
+        public $email;
+        public $subject;
+        public $body;
+        public $verifyCode;
 
-    /**
-     * @return array customized attribute labels
-     */
-    public function attributeLabels()
-    {
-        return [
-            'verifyCode' => 'Verification Code',
-        ];
-    }
-
-    /**
-     * Sends an email to the specified email address using the information collected by this model.
-     * @param string $email the target email address
-     * @return bool whether the model passes validation
-     */
-    public function contact($email)
-    {
-        if ($this->validate()) {
-            Yii::$app->mailer->compose()
-                ->setTo($email)
-                ->setFrom([$this->email => $this->name])
-                ->setSubject($this->subject)
-                ->setTextBody($this->body)
-                ->send();
-
-            return true;
+        /**
+         * @return array the validation rules.
+         */
+        public function rules()
+        {
+            return [
+                // name, email, subject and body are required
+                [
+                    [
+                        'name' ,
+                        'email' ,
+                        'subject' ,
+                        'body',
+                    ] ,
+                    'required',
+                ] ,
+                // email has to be a valid email address
+                [
+                    'email' ,
+                    'email',
+                ] ,
+                // verifyCode needs to be entered correctly
+                [
+                    'verifyCode' ,
+                    'captcha',
+                ] ,
+            ];
         }
-        return false;
+
+        /**
+         * @return array customized attribute labels
+         */
+        public function attributeLabels()
+        {
+            return [
+                'verifyCode' => 'Verification Code' ,
+            ];
+        }
+
+        /**
+         * Sends an email to the specified email address using the information collected by this model.
+         *
+         * @param string $email the target email address
+         *
+         * @return bool whether the model passes validation
+         */
+        public function contact($email)
+        {
+            if($this->validate()){
+                Yii::$app->mailer->compose()->setTo($email)->setFrom([$this->email => $this->name])
+                    ->setSubject($this->subject)->setTextBody($this->body)->send();
+
+                return TRUE;
+            }
+
+            return FALSE;
+        }
     }
-}
